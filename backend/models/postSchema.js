2bf32b54a9b53bee090bc5100358eb66310927b6
mj1999 <mayank.joshi92@yahoo.com>
2024-05-04 19:52:37 +0530

    added date created timestamps in post schema

--- backend/models/postSchema.js
+++ backend/models/postSchema.js
@@ -1,34 +1,44 @@
 const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required: true,
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    body:{
-        type:String, 
+    body: {
+      type: String,
     },
-    authorId:{
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    heroImage: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    comments: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-        required:true
-    },
-    heroImage:{
-        type:String,
-        required:true
-    },
-    images:[{
-        type:String
-    }],
-    comments:[{
+        ref: "comment",
+      },
+    ],
+    likes: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "comment"
-    }],
-    likes:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "like"
-    }]
-});
+        ref: "like",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model("post", postSchema);
 module.exports = Post;
